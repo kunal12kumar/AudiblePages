27980ff957badcd400de5acdeefc5bec5c7b67cd
kunal12kumar <23cs3035@rgipt.ac.in>
2024-11-05 23:07:51 +0530

    adding frontend page

--- audiblepages_frontend/src/index.js
+++ audiblepages_frontend/src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Uploadsection from './pages/Uploadsection';
+import Fileupload from './pages/Fileupload';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let allrouter=createBrowserRouter([
+  {
+    path:'/',
+    element:<Home></Home>
+  },
+  {
+    path:'/login',
+    element:<Login></Login>
+  },
+  {
+    path:'/SignUp',
+    element:<SignUp></SignUp>
+  },
+  {
+    path:'/UploadSection',
+    element:<Fileupload></Fileupload>
+  }
+]
+)
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={allrouter}></RouterProvider>
   </React.StrictMode>
 );
 
